343e6143ec3baa98ecfc537046a0cf960303503f
Codey Du <codeydu@hotmail.com>
2022-06-06 23:20:02 -0700

    chore: add state, getter, mutation for date query parameter

--- src/store/storeModules/covidData.js
+++ src/store/storeModules/covidData.js
@@ -1,22 +1,28 @@
+import dayjs from 'dayjs'
 export const covidData = {
   namespaced: true,
 
   state: {
     covidData: [],
-    province: { text: 'Nation Wide', value: '' }
+    province: { text: 'Nation Wide', value: '' },
+    dateStart: dayjs().subtract(1, 'month').format('YYYY-MM-DD'),
+    dateFinal: dayjs().format('YYYY-MM-DD')
   },
   getters: {
     getApiURL: state => {
       return state.province.value.length === 0
-        ? '/reports?&after=2022-05-01&before'
-        : `/reports/province/${state.province.value}?&after=2022-05-01&before`
-    }
+        ? `/reports`
+        : `/reports/province/${state.province.value}`
+    },
+    queryDateParam: state => ({
+      params: { after: state.dateStart, before: state.dateFinal }
+    })
   },
   actions: {
     async fetchData({ commit, getters }) {
       return new Promise((resolve, reject) => {
         this._vm.$_api
-          .get(getters.getApiURL)
+          .get(getters.getApiURL, getters.queryDateParam)
           .then(res => {
             commit('SET_DATA', res.data.data)
             resolve()
@@ -30,6 +36,8 @@ export const covidData = {
   },
   mutations: {
     SET_DATA: (state, data) => (state.covidData = data),
-    SET_PROVINCE: (state, province) => (state.province = province)
+    SET_PROVINCE: (state, province) => (state.province = province),
+    SET_DATE_START: (state, date) => (state.dateStart = date),
+    SET_DATE_FINAL: (state, date) => (state.dateFinal = date)
   }
 }
